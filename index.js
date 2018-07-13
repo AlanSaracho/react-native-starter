6e0e1ea7c4a7957fdd3dad15727422f7bb3acda6
Alan Saracho <alan.saracho@enerminds.com>
2018-07-13 16:27:55 -0300

    feat: add folder structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+
 import {name as appName} from './app.json';
+import App from './app/src';
 
 AppRegistry.registerComponent(appName, () => App);
