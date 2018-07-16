8ef7212d1cbd1936ed991359867ad59d9f32af4a
Alan Saracho <saracho.alan@gmail.com>
2018-07-16 17:23:43 -0300

    feat: add redux and saga

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 
 import HelloPrometium from './screens/HelloPrometium';
+import {configureStore} from './store';
 
-export default () => <HelloPrometium/>;
+const store = configureStore();
+
+export default () => (
+    <Provider store={store}>
+        <HelloPrometium/>
+    </Provider>);
